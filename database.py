93baa390f1653f0220b0ce4eb1d498b796aa0611
Mustafa Kale <116312721+mustafaklee@users.noreply.github.com>
2024-07-20 16:31:17 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,11 @@
+import pyodbc
+
+connection_string = (
+                'DRIVER={SQL Server};'
+                'SERVER=MUSTAFAKALE\\SQLEXPRESS;'  
+                'DATABASE=StokBilgileri;'
+                'Integrated Security=True'
+            )
+
+
+
